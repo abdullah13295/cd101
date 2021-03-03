1666e0c62795adb8e2f05cd3eae71594575775f4
abdullah13295 <78905386+abdullah13295@users.noreply.github.com>
2021-03-04 00:29:53 +0300

    second lesson

--- 2.py
+++ 2.py
@@ -1,48 +1,59 @@
-# lst=[1,3,4,5.3,'ali']
+# lst=[1,3,4,5.3,'ali'] #List definition
 
-# for idx,val in enumerate(lst):
-#     print(idx,val)
+# for a,b in enumerate(lst): #To write items with index numbers inside the list     # enumerate is useful for obtaining an indexed list:
+#     print(a,b)
 
-# for val in lst:
-#     print(val)
+# for val in lst:  #To write items inside the list
+#      print(val)
 
-# #if
+# if    #If the condition is met, the program will execute the instructions within the condition once, and if the condition is not met, it will ignore those instructions
 
-# x=10 # >,<,=<,=>,==,!=,and,or,not
-# print(bool(x>0 and x<12))  
+
+# >,<,=<,=>,==,!=,and,or,not   #Operations used within the condition context
+
+# x=10
+# print(bool(x>0 and x<12))   #Here it is required to write (True) if the two conditions are met: before (and) and after (and) together. And writing (False) in case at least one of the two conditions is not met
 
 
 
 # even or not 
-# x=10
-# if x%2==0:
-#     print("x is even")
+
+# x=10  #Define or enter the variable (x)
+# if x%2==0:   #Condition text (do the following if the remainder of dividing (x) by 2 equals zero)
+#     print("x is even") #Print a message that the number (x) is an even number
 # print("bye bye")
 
+
+#Example of using (if) and (else)
 # x=10
 # if x%2==0:
 #      print("x is even")
-# else:
+# else: #The program executes the instructions inside (else) ONLY in case the (if) condition, is not met and it does not need condetion
 #     print("x is not even")
 
 
-
+# this programe is for find which element of list is even and which not
 # lst=[1,3,4,5.3,15]
-# for val in lst:
+# for val in lst: # we need (for )loop to try all elements in list
 #     if val%2==0:
 #         print(f"{val} is even")
 #     else:
 #         print(f"{val} is not even")
 
 
-# lst=[1,3,4,5.3,15]
+
+#here we use more than 2condition so we need (if,elif,else)
+# lst=[1,3,4,5.3,15] 
 # for val in lst:
-#     if val%2==0:
-#         print(f"{val} is even")
-#     elif val%3==0:
-#         print(f"{val}%3 is true")
-#     else:
-#         print(f"{val} is not even")
+#      if val%2==0: 
+#          print(f"{val} is even")
+#     elif val%3==0: #if(if condition) did not met ,the program will see if (elif condition)will met , if not the programe will move to (else)
+#          print(f"{val}%3 is true")
+#     else: 
+#          print(f"{val} is not even") # the program will print it ONLY if (if,elif)will not met 
+
+
+
 
 # lst=[1,-30,4,52,15]
 # for val in lst:
@@ -51,20 +62,20 @@
 #     else:
 #         print("ERROR")
 
-# print(bool(lst))
-# a=[]
+# print(bool(lst)) #(True) because there are elements in this list
+# a=[] 
 # x=5
 # v=0
 # l=[None]
 # n=None
-# print(bool(a))
-# print(bool(x))
+# print(bool(a)) #(False) because there are no elements in this list
+# print(bool(x)) #(True) because this element not 0
 # print(bool(v))
-# print(bool(l))
+# print(bool(l)) #(True) because there are elements in this list
 # print(bool(n))
 
 
-# lst=[35,63,73,36,85,47,57,47,33,73,23,7,8,3,8,0]
+# lst=[35,63,73,36,85,47,57,47,33,73,23,7,8,3,8,0] #This program is to divide the group into two groups (even and not even numbers) and then print these lists
 # even=[]
 # n_even=[]
 # for x in lst:
@@ -76,10 +87,16 @@
 # print(even)
 # print(n_even)
 
-# print(sum(lst))
+# print(sum(lst)) #(sum) is to find(The sum of the numbers in the list)
 
-# continue & break 
 
+
+
+
+
+# continue & break #(Continue) and (break) are only used in loops
+#(Continue)is to ignore the next lines in the loop and return to the first line of loop 
+#(break)is to stop the loop 
 
 # names=[]
 # ages=[]
@@ -104,25 +121,30 @@
 #     print("you are welcome when ever you want)")
 
 
+
+
+                            #(Dictionary) The dictionary works like a list, but with the addition that an item can be called with a specific name
 # data={"a":33,"b":44,"c":35}
-# print(data["a"])
-# for key,val in data.items():
+# print(data["a"]) #HERE WE CALLED THE FIRST ELEMENT BY ("a")
+# for key,val in data.items(): #here we called all element with there names
 #     print(key,val)
+# print(data)
 # data["b"]=55
+# print(data)
 # data["z"]=12
 # print(data)
 
-# full_data={""}   #val could be a list of data
+# full_data={""}   #(val) could be a list of data
 
 
 
-#functions
-def first_foo(x,y):
-    a=x+y
-    print(a)   #no element saved
-first_foo(7,8)
+#functions 
+# def first_foo(x,y):
+#     a=x+y
+#     print(a)   #no element saved
+# first_foo(7,8) #here I want to do the opperations in(first_foo) on (7,8)
 
-def first_fooo(x,y):
-    a=x+y
-    return a #save result in mamory 
+# def first_fooo(x,y):
+#     a=x+y
+#     return a #save result in mamory 
 
